c2c9f4ef456f5f49cec771395ae667192aa08dc7
Laura López <lauralopez166@gmail.com>
2023-10-13 11:33:49 +0200

    css acabado

---
+++ script.js
@@ -0,0 +1,6 @@
+const depositado = document.getElementById("depositado");
+const saldo = document.getElementById("saldo");
+const ingresar = parseFloat(document.getElementById("ingresar").value);
+const retirar = document.getElementById("retirar");
+const depositar = document.getElementById("depositar");
+
